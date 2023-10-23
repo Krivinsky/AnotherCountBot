16c65f866dcb29b8719e27427f0c20dd948c8bf0
Eugene Krivinsky <Playa313@yandex.ru>
2023-10-23 22:30:34 +0300

    выполнены задания 11.2.4 и 11.2.5

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace AnotherCountBot
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+namespace AnotherCountBot
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureSevices(services))   // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build();
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureSevices(IServiceCollection services)
+        {
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6942178033:AAFtIh5FvhplhcNw1LBc-Sqa3y3EKBDjh04"));
+
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
         }
     }
 }
